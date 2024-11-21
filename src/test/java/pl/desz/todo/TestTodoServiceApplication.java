ba7c10c5c6f6e31cac99d62d3879978fe6135b9c
Denis <denisszczukocki@Deniss-MacBook-Pro-2.local>
2024-11-21 18:02:09 +0100

    add initial version

---
+++ src/test/java/pl/desz/todo/TestTodoServiceApplication.java
@@ -0,0 +1,11 @@
+package pl.desz.todo;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestTodoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(TodoServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
